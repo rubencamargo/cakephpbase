7400fde3ea6232e0b364aa4e44d6592145dbb74e
Ruben Camargo <rubencamargo@gmail.com>
2023-08-05 10:56:01 -0300

    Actualización y menú.

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,23 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Admin;
+
+use App\Controller\AppController;
+
+/**
+ * Dashboard Controller
+ */
+class DashboardController extends AppController
+{
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|null|void Renders view
+     */
+    public function index()
+    {
+
+    }
+
+}
--- templates/element/sidebar.php
+++ templates/element/sidebar.php
@@ -2,19 +2,40 @@
     <div>
         <ul class="nav nav-pills flex-column mb-auto">
             <li class="nav-item">
-                <a class="nav-link link-light active" aria-current="page" href="#">
-                    <span data-feather="home" class="align-text-bottom"></span>
-                    <i class="bi-speedometer2"></i> Dashboard
-                </a>
+                <?php
+                if ($this->name == "Dashboard") {
+                    echo $this->Html->link('<i class="bi-speedometer2"></i> ' . 'Dashboard', '/admin/dashboard', ['class' => 'nav-link link-light active', 'escape' => false]);
+                } else {
+                    echo $this->Html->link('<i class="bi-speedometer2"></i> ' . 'Dashboard', '/admin/dashboard', ['class' => 'nav-link link-light', 'escape' => false]);
+                }
+                ?>
             </li>
             <li class="nav-item">
-                <?php echo $this->Html->link('<i class="bi-person-vcard"></i> ' . 'Roles', '/admin/roles', ['class' => 'nav-link link-light', 'escape' => false]); ?>
+                <?php
+                if ($this->name == "Roles") {
+                    echo $this->Html->link('<i class="bi-person-vcard"></i> ' . 'Roles', '/admin/roles', ['class' => 'nav-link link-light active', 'escape' => false]);
+                } else {
+                    echo $this->Html->link('<i class="bi-person-vcard"></i> ' . 'Roles', '/admin/roles', ['class' => 'nav-link link-light', 'escape' => false]);
+                }
+                ?>
             </li>
             <li class="nav-item">
-                <?php echo $this->Html->link('<i class="bi-people"></i> ' . 'Users', '/admin/users', ['class' => 'nav-link link-light', 'escape' => false]); ?>
+                <?php
+                if ($this->name == "Users") {
+                    echo $this->Html->link('<i class="bi-people"></i> ' . 'Users', '/admin/users', ['class' => 'nav-link link-light active', 'escape' => false]);
+                } else {
+                    echo $this->Html->link('<i class="bi-people"></i> ' . 'Users', '/admin/users', ['class' => 'nav-link link-light', 'escape' => false]);
+                }
+                ?>
             </li>
             <li class="nav-item">
-                <?php echo $this->Html->link('<i class="bi-flag"></i> ' . 'Countries', '/admin/countries', ['class' => 'nav-link link-light', 'escape' => false]); ?>
+                <?php
+                if ($this->name == "Countries") {
+                    echo $this->Html->link('<i class="bi-flag"></i> ' . 'Countries', '/admin/countries', ['class' => 'nav-link link-light active', 'escape' => false]);
+                } else {
+                    echo $this->Html->link('<i class="bi-flag"></i> ' . 'Countries', '/admin/countries', ['class' => 'nav-link link-light', 'escape' => false]);
+                }
+                ?>
             </li>
         </ul>
     </div>
